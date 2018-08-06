9e7f852af1990d2511847b910eda192ac98d6b6e
Dominic Jones <haakym@gmail.com>
2018-08-06 19:47:48 +0100

    refactor download certificate test with teardown

--- tests/Feature/DownloadCertificateTest.php
+++ tests/Feature/DownloadCertificateTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature;
 
-use App\Models\Certificate;
+use Mockery;
+use Tests\TestCase;
 use App\Models\Instructor;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Certificate;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithExceptionHandling;
 
 class DownloadCertificateTest extends TestCase
@@ -15,24 +16,34 @@ class DownloadCertificateTest extends TestCase
     use RefreshDatabase;
     use InteractsWithExceptionHandling;
 
+    public function setUp(){
+        parent::setUp();
+        Storage::fake('local');
+        Storage::disk('local')->put(
+            'certificates/tennis-qualification.pdf',
+            file_get_contents(base_path('tests/__fixtures__/tennis-coach-qualification.pdf'))
+        );
+    }
+    
+    public function tearDown(){
+        Storage::delete('certificates/tennis-qualification.pdf');
+        parent::tearDown();
+        Mockery::close();
+    }
+
     /** @test */
     public function user_can_download_an_instructors_certificate()
     {
-        // $this->withoutExceptionHandling();
-        Storage::fake('local');
-        $filename = 'certificates/tennis-qualification.pdf';
-        Storage::put($filename, 'Contents');
-        Storage::assertExists($filename); // check the file exists, assert the arrange step
-        
         $instructor = factory(Instructor::class)->states('volunteer')->create();
         $certificate = factory(Certificate::class)->states('qualification')->create([
             'name' => 'Tennis Coach Trainer',
-            'file' => $filename,
+            'file' => 'certificates/tennis-qualification.pdf',
             'instructor_id' => $instructor->id,
         ]);
 
         $response = $this->get("/certificates/{$certificate->id}/download");
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertHeader('Content-Type', 'application/pdf');
     }
 }
