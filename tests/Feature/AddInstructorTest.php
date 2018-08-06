baec14868ef89b137a17c6b44334bbbb5fe8e62e
Dominic Jones <haakym@gmail.com>
2018-08-06 23:03:34 +0100

    add coach test passing

--- tests/Feature/AddInstructorTest.php
+++ tests/Feature/AddInstructorTest.php
@@ -29,4 +29,25 @@ class AddInstructorTest extends TestCase
         $this->assertEquals('volunteer', $instructor->type);
         $this->assertEquals(0, $instructor->hourly_rate);
     }
+
+    /** @test */
+    public function user_can_add_a_coach()
+    {
+        $response = $this->post('instructors', [
+            'name' => 'Jane Doeseph',
+            'email' => 'jane@example.com',
+            'type' => 'coach',
+            'hourly_rate' => '2000',
+        ]);
+
+        $instructor = Instructor::first();
+
+        $response->assertRedirect("instructors/{$instructor->id}")
+            ->assertSessionHas('message', "New {$instructor->type} {$instructor->name} added.");
+        
+        $this->assertEquals('Jane Doeseph', $instructor->name);
+        $this->assertEquals('jane@example.com', $instructor->email);
+        $this->assertEquals('coach', $instructor->type);
+        $this->assertEquals(2000, $instructor->hourly_rate);
+    }
 }
