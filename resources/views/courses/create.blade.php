1711cb0a20e43ad46808ef17ea385cf122927f13
Dominic Jones <haakym@gmail.com>
2018-08-11 18:44:34 +0100

    move javascript to partial where it's used

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.app')
 
-@section('head')
-    @parent
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-@endsection
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -100,44 +95,3 @@
     </div>
 </div>
 @endsection
-
-@push('scripts')
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-    <script>
-    $(function() {
-        let dateFromOld = '{{ old("date_from") }}';
-        let dateToOld = '{{ old("date_to") }}';
-        let dateFrom = $('input[name="date_from"]').daterangepicker({
-            'singleDatePicker': true,
-            'opens': 'center',
-            'drops': 'up',
-            'locale': {
-               'format': 'DD-MM-YYYY'
-            },
-            'startDate': dateFromOld ? dateFromOld : moment().add(1, 'days'),
-            'minDate': moment().add(1, 'days'),
-            'maxDate': moment().add(1, 'year'),
-        }, function(start, end, label) {
-            // TODO
-            // if dateTo < start, set dateTo = start
-        });
-
-        let dateTo = $('input[name="date_to"]').daterangepicker({
-            'singleDatePicker': true,
-            'opens': 'center',
-            'drops': 'up',
-            'locale': {
-               'format': 'DD-MM-YYYY'
-            },
-            'startDate': dateToOld ? dateToOld : moment().add(5, 'days'),
-            'minDate': moment().add(1, 'days'),
-            'maxDate': moment().add(1, 'year'),
-        }, function(start, end, label) {
-            // TODO
-            // if dateFrom > start, set dateFrom = start
-        });
-    });
-    </script>
-@endpush
--- resources/views/courses/partials/assign-instructor.blade.php
+++ resources/views/courses/partials/assign-instructor.blade.php
@@ -1,3 +1,8 @@
+@section('head')
+    @parent
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+@endsection
+
 <div class="card" style="margin-top:1em;">
     <div class="card-body">
         <div class="card-title">
@@ -50,3 +55,44 @@
         </form>
     </div>
 </div>
+
+@push('scripts')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script>
+    $(function() {
+        let dateFromOld = '{{ old("date_from") }}';
+        let dateToOld = '{{ old("date_to") }}';
+        let dateFrom = $('input[name="date_from"]').daterangepicker({
+            'singleDatePicker': true,
+            'opens': 'center',
+            'drops': 'up',
+            'locale': {
+               'format': 'DD-MM-YYYY'
+            },
+            'startDate': dateFromOld ? dateFromOld : moment().add(1, 'days'),
+            'minDate': moment().add(1, 'days'),
+            'maxDate': moment().add(1, 'year'),
+        }, function(start, end, label) {
+            // TODO
+            // if dateTo < start, set dateTo = start
+        });
+
+        let dateTo = $('input[name="date_to"]').daterangepicker({
+            'singleDatePicker': true,
+            'opens': 'center',
+            'drops': 'up',
+            'locale': {
+               'format': 'DD-MM-YYYY'
+            },
+            'startDate': dateToOld ? dateToOld : moment().add(5, 'days'),
+            'minDate': moment().add(1, 'days'),
+            'maxDate': moment().add(1, 'year'),
+        }, function(start, end, label) {
+            // TODO
+            // if dateFrom > start, set dateFrom = start
+        });
+    });
+    </script>
+@endpush
