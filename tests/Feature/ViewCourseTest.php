9c220e587f81d6cb8426640100180ad7b2af08ea
Dominic Jones <haakym@gmail.com>
2018-08-11 21:14:10 +0100

    fix failing user_can_view_a_course

--- tests/Feature/ViewCourseTest.php
+++ tests/Feature/ViewCourseTest.php
@@ -15,12 +15,15 @@ class ViewCourseTest extends TestCase
     /** @test */
     public function user_can_view_a_course()
     {
+        $dateFrom = Carbon::parse('first day of January next year');
+        $dateTo = $dateFrom->copy()->addDay();
+
         $course = factory(Course::class)->create([
             'name' => 'Beginner skills summer camp',
             'description' => 'Football training for beginners ages 11-13',
             'address' => 'SuperSkills Soccer UK Ltd, Bridge Rd, Wembley HA9 9JP',
-            'date_from' => Carbon::parse('September 10, 2018'),
-            'date_to' => Carbon::parse('September 12, 2018'),
+            'date_from' => $dateFrom->format('Y-m-d'),
+            'date_to' => $dateTo->format('Y-m-d'),
         ]);
 
         $response = $this->get("/courses/{$course->id}");
@@ -29,7 +32,6 @@ class ViewCourseTest extends TestCase
         $response->assertSee('Beginner skills summer camp');
         $response->assertSee('Football training for beginners ages 11-13');
         $response->assertSee('SuperSkills Soccer UK Ltd, Bridge Rd, Wembley HA9 9JP');
-        $response->assertSee('September 10, 2018');
-        $response->assertSee('September 12, 2018');
+        $response->assertSee($dateFrom->format('d-m-Y') . ' to ' .$dateTo->format('d-m-Y'));
     }
 }
