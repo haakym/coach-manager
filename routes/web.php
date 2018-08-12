f46cdc24b54c2f6fc2a53675682870588623f29f
Dominic Jones <haakym@gmail.com>
2018-08-12 13:06:53 +0100

    add resources to nav

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,14 @@ Route::get('/', function () {
 Route::get('/calendar', 'CalendarController@index')->name('calendar.index');
 Route::get('/calendar/data', 'CalendarController@data');
 
-Route::get('/courses/create', 'CourseController@create');
+Route::get('/courses/create', 'CourseController@create')->name('courses.create');
 Route::post('/courses', 'CourseController@store');
 Route::get('/courses/{course}/edit', 'CourseController@edit');
 Route::post('/courses/{course}/instructors', 'CourseInstructorController@store')->name('courses.assign.instructor');
 Route::get('/courses/{course}', 'CourseController@show')->name('courses.show');
 Route::put('/courses/{course}', 'CourseController@update');
 
-Route::get('/instructors/create', 'InstructorController@create');
+Route::get('/instructors/create', 'InstructorController@create')->name('instructors.create');
 Route::post('/instructors', 'InstructorController@store');
 Route::post('/instructors/{instructor}/certificates', 'InstructorCertificateController@store');
 Route::get('/instructors/{instructor}/edit', 'InstructorController@edit');
@@ -34,10 +34,10 @@ Route::put('/instructors/{instructor}', 'InstructorController@update');
 
 Route::get('/certificates/{certificate}/download', 'CertificateDownloadController')->name('certificates.download');
 
-Route::get('resources/courses', 'DataTable\CourseController@index');
+Route::get('resources/courses', 'DataTable\CourseController@index')->name('datatables.courses.index');
 Route::get('resources/courses/data', 'DataTable\CourseController@data')->name('datatables.courses.data');
 
-Route::get('resources/instructors', 'DataTable\InstructorController@index');
+Route::get('resources/instructors', 'DataTable\InstructorController@index')->name('datatables.instructors.index');
 Route::get('resources/instructors/data', 'DataTable\InstructorController@data')->name('datatables.instructors.data');
 
 Auth::routes();
