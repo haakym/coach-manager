a3dacf0055e5bb61fd42e5de0743f200e285599f
Dominic Jones <haakym@gmail.com>
2018-08-12 13:09:11 +0100

    tidy up routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'CalendarController@index');
 
 Route::get('/calendar', 'CalendarController@index')->name('calendar.index');
 Route::get('/calendar/data', 'CalendarController@data');
@@ -39,7 +26,3 @@ Route::get('resources/courses/data', 'DataTable\CourseController@data')->name('d
 
 Route::get('resources/instructors', 'DataTable\InstructorController@index')->name('datatables.instructors.index');
 Route::get('resources/instructors/data', 'DataTable\InstructorController@data')->name('datatables.instructors.data');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
