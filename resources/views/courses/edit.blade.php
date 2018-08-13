7ef40ffc9916696085331481b94faf4d4fc44c0c
Dominic Jones <haakym@gmail.com>
2018-08-13 02:37:00 +0100

    missing datepicker

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('head')
+    @parent
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -99,3 +104,43 @@
     </div>
 </div>
 @endsection
+
+@push('scripts')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script>
+    $(function() {
+        let dateFromOld = '{{ old("date_from") ? old("date_from") : $course->date_from->format("d-m-Y") }}';
+        let dateToOld = '{{ old("date_to") ? old("date_to") : $course->date_to->format("d-m-Y") }}';
+        let dateFrom = $('input[name="date_from"]').daterangepicker({
+            'singleDatePicker': true,
+            'opens': 'center',
+            'drops': 'up',
+            'locale': {
+               'format': 'DD-MM-YYYY'
+            },
+            'startDate': dateFromOld ? dateFromOld : moment().add(1, 'days'),
+            'minDate': moment().add(1, 'days'),
+            'maxDate': moment().add(1, 'year'),
+        }, function(start, end, label) {
+            // TODO
+            // if dateTo < start, set dateTo = start
+        });
+
+        let dateTo = $('input[name="date_to"]').daterangepicker({
+            'singleDatePicker': true,
+            'opens': 'center',
+            'drops': 'up',
+            'locale': {
+               'format': 'DD-MM-YYYY'
+            },
+            'startDate': dateToOld ? dateToOld : moment().add(5, 'days'),
+            'minDate': moment().add(1, 'days'),
+            'maxDate': moment().add(1, 'year'),
+        }, function(start, end, label) {
+            // TODO
+            // if dateFrom > start, set dateFrom = start
+        });
+    });
+    </script>
+@endpush
